4b73407dc1051101947931209c49874c450acfc6
jayyi <jayyi@tencent.com>
2024-01-18 15:04:59 +0800

    Add unit tests

--- Makefile
+++ Makefile
@@ -1,18 +1,7 @@
 # Package gomooon
 # Wrote by yijian on 2024/01/04
 
-all: test hmac_sha256_sign
-
-test: main.go
-	go build -o $@ main.go
-
-hmac_sha256_sign: hmac_sha256_sign.go
-	go build -o $@ $<
-
-.PHONY: clean tidy
-
-clean:
-	rm -f test hmac_sha256_sign
+.PHONY: tidy
 
 tidy:
 	go mod tidy
---
+++ utils/str_utils_test.go
@@ -0,0 +1,75 @@
+// Package utils
+// Wrote by yijian on 2024/01/18
+package utils
+
+import (
+	"os"
+	"testing"
+)
+
+// go test -v -run="TestGetNonceStr"
+func TestGetNonceStr(t *testing.T) {
+	// 测试长度 1
+	strLen := 1
+	str := GetNonceStr(strLen)
+	if len(str) == strLen {
+		t.Logf("EXCEPTED: %s\n", str)
+	} else {
+		t.Errorf("UNEXCEPTED: %s\n",str)
+	}
+
+	// 测试长度 28
+	strLen = 28
+	str = GetNonceStr(28)
+	if len(str) == strLen {
+		t.Logf("EXCEPTED: %s\n", str)
+	} else {
+		t.Errorf("UNEXCEPTED: %s\n",str)
+	}
+}
+
+// 身份证号隐私数据，执行时指定
+// go test -v -run="TestIsResidentIdentityCardNumber" -args FLAG ID // FLAG 只能取值 0 或者 1，1 表示 ID 为无效身份证号，0 表示为有效的身份证号
+func TestIsResidentIdentityCardNumber(t *testing.T) {
+	flag := os.Args[len(os.Args)-2]
+	id := os.Args[len(os.Args)-1]
+	if IsResidentIdentityCardNumber(id) {
+		if flag == "0" {
+			t.Errorf("%s is ID number\n", id)
+		} else {
+			t.Logf("%s is ID number\n", id)
+		}
+	} else {
+		if flag == "0" {
+			t.Logf("%s is not ID number\n", id)
+		} else {
+			t.Errorf("%s is not ID number\n", id)
+		}
+	}
+}
+
+// 前面的密钥需要命令行传入
+// go test -v -run="TestUpperHmacSHA256Sign" -args KEY DATA
+func TestUpperHmacSHA256Sign(t *testing.T) {
+	key := os.Args[len(os.Args)-2]
+	data := os.Args[len(os.Args)-1]
+	signature, err := UpperHmacSHA256Sign(data, key)
+	if err != nil {
+		t.Errorf("%s\n", err.Error())
+	} else {
+		t.Logf("signature: %s\n", signature)
+	}
+}
+
+// 前面的密钥需要命令行传入
+// go test -v -run="TestLowerHmacSHA256Sign" -args KEY DATA
+func TestLowerHmacSHA256Sign(t *testing.T) {
+	key := os.Args[len(os.Args)-2]
+	data := os.Args[len(os.Args)-1]
+	signature, err := LowerHmacSHA256Sign(data, key)
+	if err != nil {
+		t.Errorf("%s\n", err.Error())
+	} else {
+		t.Logf("signature: %s\n", signature)
+	}
+}

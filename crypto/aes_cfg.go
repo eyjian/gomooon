3df21fc4d45b53b9bb7d3d3514e0e914e64852af
蓝天 <eyjian@qq.com>
2024-01-25 21:50:57 +0800

    Add AesCFBEncryptText  & AesCFBDecryptText

---
+++ crypto/aes_cfg.go
@@ -0,0 +1,83 @@
+// Package crypto
+// Wrote by yijian on 2024/01/25
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+// 与ECB和CBC模式只能够加密块数据不同，CFB能够将块密文（Block Cipher）转换为流密文（Stream Cipher）。
+// AES 要求 key 长度只能为 128 或 192 或 256 比特中的一种，即 16 字节或 24 字节或 32 字节中的一种。
+
+// AesCFBEncryptText 加密文本
+// key 加密密钥，长度不能超过 32 个字符，否则返回 error。如果不是 16、24 和 32 整的倍数，则会自动补 0 填充到最近的长度。
+func AesCFBEncryptText(key, data string) (string, error) {
+	keyLen := len(key)
+	if keyLen > 256 {
+		return "", fmt.Errorf("length of key exceeds 256")
+	}
+
+	// 创建 AES 分组密码的实例
+	keyBytes := []byte(padToLength(key))
+
+	// 创建AES密钥
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// 初始化向量
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	// 创建CFB模式的加密器
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	// 加密明文
+	plaintext := []byte(data)
+	ciphertext := make([]byte, len(plaintext))
+	stream.XORKeyStream(ciphertext, plaintext)
+
+	return hex.EncodeToString(ciphertext), nil
+}
+
+// AesCFBDecryptText 解密文本
+// key 解密密钥，值同加密密钥，长度不能超过 32 个字符，否则返回 error。如果不是 16、24 和 32 整的倍数，则会自动补 0 填充到最近的长度。
+func AesCFBDecryptText(key, data string) (string, error) {
+	keyLen := len(key)
+	if keyLen > 256 {
+		return "", fmt.Errorf("length of key exceeds 256")
+	}
+
+	// 创建 AES 分组密码的实例
+	keyBytes := []byte(padToLength(key))
+
+	// 创建AES密钥
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// 初始化向量
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	// 创建CFB模式的解密器
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	// 解密密文
+	ciphertext, _ := hex.DecodeString(data)
+	plaintext := make([]byte, len(ciphertext))
+	stream.XORKeyStream(plaintext, ciphertext)
+
+	return string(plaintext), nil
+}
---
+++ crypto/aes_cfg_test.go
@@ -0,0 +1,43 @@
+// Package crypto
+// Wrote by yijian on 2024/01/25
+package crypto
+
+import "testing"
+
+// go test -v -run="TestAesCFBEncryptText"
+func TestAesCFBEncryptText(t *testing.T) {
+	key := "0123456789"
+	data := "gomooon"
+
+	if testAesCFBEncryptText(t, key, data) {
+		key = "0123456789123456"
+		if testAesCFBEncryptText(t, key, data) {
+			key = "0123456789123456aa"
+			data = "hello, welcome"
+			if testAesCFBEncryptText(t, key, data) {
+				key := "0123456789012345678912345"
+				_ = testAesCFBEncryptText(t, key, data)
+			}
+		}
+	}
+}
+func testAesCFBEncryptText(t *testing.T, key, data string) bool {
+	// 加密
+	encodedCiphertext, err := AesCFBEncryptText(key, data)
+	if err != nil {
+		t.Errorf("AesCFBEncryptText error: %s\n", err.Error())
+		return false
+	} else {
+		t.Logf("AesCFBEncryptText ok: %s\n", encodedCiphertext)
+
+		// 解密
+		plaintext, err := AesCFBEncryptText(key, encodedCiphertext)
+		if err != nil {
+			t.Errorf("AesCFBEncryptText error: %s\n", err.Error())
+			return false
+		} else {
+			t.Logf("AesCFBEncryptText ok: %s\n", plaintext)
+			return true
+		}
+	}
+}

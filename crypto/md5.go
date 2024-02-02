e720e3e324e6e421ea0c0c6cb93efaed86d66b15
jayyi <jayyi@tencent.com>
2024-02-02 19:59:27 +0800

    Rename Md5Sign to Md5Sum

--- crypto/md5.go
+++ crypto/md5.go
@@ -3,14 +3,18 @@
 package crypto
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-	"strings"
+    "crypto/md5"
+    "encoding/hex"
+    "strings"
 )
 
-// Md5Sign MD5 签名
-// data 需要签名的数据
-func Md5Sign(data string) string {
-	hash := md5.Sum([]byte(data))
-	return strings.ToUpper(hex.EncodeToString(hash[:]))
+// Md5Sum MD5 计算
+// data 需要计算的数据
+func Md5Sum(data string, toUpper bool) string {
+    hash := md5.Sum([]byte(data))
+    if toUpper {
+        return strings.ToUpper(hex.EncodeToString(hash[:]))
+    } else {
+        return strings.ToLower(hex.EncodeToString(hash[:]))
+    }
 }

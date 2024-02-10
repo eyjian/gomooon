23d3e02507c80c137670eaef1d47b7817ef40476
eyjian <eyjian@qq.com>
2024-02-10 22:59:27 +0800

    Add DesensitizeResidentIdentityCardNumber

--- utils/str_utils.go
+++ utils/str_utils.go
@@ -3,134 +3,163 @@
 package utils
 
 import (
-	"math/rand"
-	"regexp"
-	"strconv"
-	"sync"
-	"time"
+    "errors"
+    "math/rand"
+    "regexp"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
 )
 
 const allCharset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const hexCharset = "abcdefABCDEF0123456789"
 
 var (
-	r      *rand.Rand
-	mu     sync.Mutex
-	once   sync.Once
-	buffer sync.Pool
+    r      *rand.Rand
+    mu     sync.Mutex
+    once   sync.Once
+    buffer sync.Pool
 )
 
 func init() {
-	source := rand.NewSource(time.Now().UnixNano())
-	r = rand.New(source)
-	buffer.New = func() interface{} {
-		return make([]byte, 0, 64)
-	}
+    source := rand.NewSource(time.Now().UnixNano())
+    r = rand.New(source)
+    buffer.New = func() interface{} {
+        return make([]byte, 0, 64)
+    }
 }
 
 func getNonceStr(length int, charset string) string {
-	once.Do(func() {
-		mu.Lock()
-		defer mu.Unlock()
-		r.Seed(time.Now().UnixNano())
-	})
+    once.Do(func() {
+        mu.Lock()
+        defer mu.Unlock()
+        r.Seed(time.Now().UnixNano())
+    })
 
-	mu.Lock()
-	defer mu.Unlock()
+    mu.Lock()
+    defer mu.Unlock()
 
-	// Get a buffer from the pool and reset its length to the desired value
-	buf := buffer.Get().([]byte)[:length]
+    // Get a buffer from the pool and reset its length to the desired value
+    buf := buffer.Get().([]byte)[:length]
 
-	for i := range buf {
-		buf[i] = charset[r.Intn(len(charset))]
-	}
+    for i := range buf {
+        buf[i] = charset[r.Intn(len(charset))]
+    }
 
-	// Convert the buffer to a string, put it back into the pool, and return the result
-	result := string(buf)
-	buffer.Put(buf)
-	return result
+    // Convert the buffer to a string, put it back into the pool, and return the result
+    result := string(buf)
+    buffer.Put(buf)
+    return result
 }
 
 func GetNonceStr(length int) string {
-	return getNonceStr(length, allCharset)
+    return getNonceStr(length, allCharset)
 }
 
 func GetHexNonceStr(length int) string {
-	return getNonceStr(length, hexCharset)
+    return getNonceStr(length, hexCharset)
 }
 
-// IsResidentIdentityCardNumber 判断是否为身份证号
+// DesensitizeResidentIdentityCardNumber 脱敏居民身份证号
+func DesensitizeResidentIdentityCardNumber(id string, m, n int) (string, error) {
+    idLength := len(id)
+    if idLength != 15 && idLength != 18 { // 不是居民身份证号
+        return "", errors.New("invalid resident identity card number")
+    }
+
+    // 显示前 m 位
+    start := 0
+    if m > idLength {
+        m = idLength
+    }
+    visibleStart := id[start:m]
+
+    // 显示后 n 位
+    end := idLength - n
+    if end < 0 {
+        end = 0
+    }
+    visibleEnd := id[end:]
+
+    // 生成脱敏后的身份证号
+    masked := visibleStart + strings.Repeat("*", idLength-m-n) + visibleEnd
+
+    return masked, nil
+}
+
+// IsResidentIdentityCardNumber 判断是否为居民身份证号
 func IsResidentIdentityCardNumber(id string) bool {
-	// 身份证号码的正则表达式
-	pattern15 := `^\d{15}$`
-	pattern18 := `^\d{17}(\d|X|x)$`
-	reg15 := regexp.MustCompile(pattern15)
-	reg18 := regexp.MustCompile(pattern18)
-	is15 := reg15.MatchString(id)
-	is18 := reg18.MatchString(id)
+    // 身份证号码的正则表达式
+    pattern15 := `^\d{15}$`
+    pattern18 := `^\d{17}(\d|X|x)$`
+    reg15 := regexp.MustCompile(pattern15)
+    reg18 := regexp.MustCompile(pattern18)
+    is15 := reg15.MatchString(id)
+    is18 := reg18.MatchString(id)
 
-	if !is15 && !is18 {
-		return false
-	}
+    if !is15 && !is18 {
+        return false
+    }
 
-	// 提取出生日期
-	var birthYear, birthMonth, birthDay int
-	if is15 {
-		birthYear, _ = strconv.Atoi("19" + id[6:8])
-		birthMonth, _ = strconv.Atoi(id[8:10])
-		birthDay, _ = strconv.Atoi(id[10:12])
-	} else {
-		birthYear, _ = strconv.Atoi(id[6:10])
-		birthMonth, _ = strconv.Atoi(id[10:12])
-		birthDay, _ = strconv.Atoi(id[12:14])
-	}
-	birthDate := time.Date(birthYear, time.Month(birthMonth), birthDay, 0, 0, 0, 0, time.UTC)
-	if birthDate.Year() != birthYear || birthDate.Month() != time.Month(birthMonth) || birthDate.Day() != birthDay {
-		return false
-	}
+    // 提取出生日期
+    var birthYear, birthMonth, birthDay int
+    if is15 {
+        birthYear, _ = strconv.Atoi("19" + id[6:8])
+        birthMonth, _ = strconv.Atoi(id[8:10])
+        birthDay, _ = strconv.Atoi(id[10:12])
+    } else {
+        birthYear, _ = strconv.Atoi(id[6:10])
+        birthMonth, _ = strconv.Atoi(id[10:12])
+        birthDay, _ = strconv.Atoi(id[12:14])
+    }
+    birthDate := time.Date(birthYear, time.Month(birthMonth), birthDay, 0, 0, 0, 0, time.UTC)
+    if birthDate.Year() != birthYear || birthDate.Month() != time.Month(birthMonth) || birthDate.Day() != birthDay {
+        return false
+    }
 
-	// 如果是18位身份证，需要检查校验码
-	if is18 {
-		weights := []int{7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2}
-		checkSum := 0
-		for i := 0; i < 17; i++ {
-			num, _ := strconv.Atoi(string(id[i]))
-			checkSum += num * weights[i]
-		}
-		checkCodes := "10X98765432"
-		return string(checkCodes[checkSum%11]) == id[17:]
-	}
+    // 如果是18位身份证，需要检查校验码
+    if is18 {
+        weights := []int{7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2}
+        checkSum := 0
+        for i := 0; i < 17; i++ {
+            num, _ := strconv.Atoi(string(id[i]))
+            checkSum += num * weights[i]
+        }
+        checkCodes := "10X98765432"
+        return string(checkCodes[checkSum%11]) == id[17:]
+    }
 
-	return true
+    return true
 }
 
 // IsValidBirthdate 判断是否为有效的出生日期
 func IsValidBirthdate(date string) bool {
-	// 使用正则表达式匹配日期格式
-	pattern := `^(\d{4})-(\d{2})-(\d{2})$`
-	matched, err := regexp.MatchString(pattern, date)
-	if err != nil {
-		return false
-	}
-	if !matched {
-		return false
-	}
+    // 使用正则表达式匹配日期格式
+    pattern := `^(\d{4})-(\d{2})-(\d{2})$`
+    matched, err := regexp.MatchString(pattern, date)
+    if err != nil {
+        return false
+    }
+    if !matched {
+        return false
+    }
 
-	// 解析日期字符串
-	t, err := time.Parse("2006-01-02", date)
-	if err != nil {
-		return false
-	}
+    // 解析日期字符串
+    t, err := time.Parse("2006-01-02", date)
+    if err != nil {
+        return false
+    }
 
-	// 检查日期是否在合理范围内
-	now := time.Now()
-	minAge := 0
-	maxAge := 120
-	minBirthYear := now.Year() - maxAge
-	maxBirthYear := now.Year() - minAge
-	if t.Year() < minBirthYear || t.Year() > maxBirthYear {
-		return false
-	}
+    // 检查日期是否在合理范围内
+    now := time.Now()
+    minAge := 0
+    maxAge := 120
+    minBirthYear := now.Year() - maxAge
+    maxBirthYear := now.Year() - minAge
+    if t.Year() < minBirthYear || t.Year() > maxBirthYear {
+        return false
+    }
 
-	return true
+    return true
 }
--- utils/str_utils_test.go
+++ utils/str_utils_test.go
@@ -3,47 +3,80 @@
 package utils
 
 import (
-	"os"
-	"testing"
+    "os"
+    "testing"
 )
 
 // go test -v -run="TestGetNonceStr"
 func TestGetNonceStr(t *testing.T) {
-	// 测试长度 1
-	strLen := 1
-	str := GetNonceStr(strLen)
-	if len(str) == strLen {
-		t.Logf("EXCEPTED: %s\n", str)
-	} else {
-		t.Errorf("UNEXCEPTED: %s\n", str)
-	}
+    // 测试长度 1
+    strLen := 1
+    str := GetNonceStr(strLen)
+    if len(str) == strLen {
+        t.Logf("EXCEPTED: %s\n", str)
+    } else {
+        t.Errorf("UNEXCEPTED: %s\n", str)
+    }
 
-	// 测试长度 28
-	strLen = 28
-	str = GetNonceStr(28)
-	if len(str) == strLen {
-		t.Logf("EXCEPTED: %s\n", str)
-	} else {
-		t.Errorf("UNEXCEPTED: %s\n", str)
-	}
+    // 测试长度 28
+    strLen = 28
+    str = GetNonceStr(28)
+    if len(str) == strLen {
+        t.Logf("EXCEPTED: %s\n", str)
+    } else {
+        t.Errorf("UNEXCEPTED: %s\n", str)
+    }
+}
+
+// go test -v -run="TestDesensitizeResidentIdentityCardNumber"
+func TestDesensitizeResidentIdentityCardNumber(t *testing.T) {
+    id := "11204416541220243X"
+    mask, err := DesensitizeResidentIdentityCardNumber(id, 2, 2)
+    if err != nil {
+        t.Errorf("id `%s` error: %s\n", id, err.Error())
+    } else {
+        t.Logf("id `%s` => mask: %s\n", id, mask)
+    }
+    
+    mask, err = DesensitizeResidentIdentityCardNumber(id, 3, 2)
+    if err != nil {
+        t.Errorf("id `%s` error: %s\n", id, err.Error())
+    } else {
+        t.Logf("id `%s` => mask: %s\n", id, mask)
+    }
+
+    mask, err = DesensitizeResidentIdentityCardNumber(id, 2, 1)
+    if err != nil {
+        t.Errorf("id `%s` error: %s\n", id, err.Error())
+    } else {
+        t.Logf("id `%s` => mask: %s\n", id, mask)
+    }
+
+    id = "112044165412202"
+    mask, err = DesensitizeResidentIdentityCardNumber(id, 2, 2)
+    if err != nil {
+        t.Errorf("id `%s` error: %s\n", id, err.Error())
+    } else {
+        t.Logf("id `%s` => mask: %s\n", id, mask)
+    }
 }
 
 // 身份证号隐私数据，执行时指定
 // go test -v -run="TestIsResidentIdentityCardNumber" -args FLAG ID // FLAG 只能取值 0 或者 1，1 表示 ID 为无效身份证号，0 表示为有效的身份证号
 func TestIsResidentIdentityCardNumber(t *testing.T) {
-	flag := os.Args[len(os.Args)-2]
-	id := os.Args[len(os.Args)-1]
-	if IsResidentIdentityCardNumber(id) {
-		if flag == "0" {
-			t.Errorf("%s is ID number\n", id)
-		} else {
-			t.Logf("%s is ID number\n", id)
-		}
-	} else {
-		if flag == "0" {
-			t.Logf("%s is not ID number\n", id)
-		} else {
-			t.Errorf("%s is not ID number\n", id)
-		}
-	}
+    flag := os.Args[len(os.Args)-2]
+    id := os.Args[len(os.Args)-1]
+    if IsResidentIdentityCardNumber(id) {
+        if flag == "0" {
+            t.Errorf("%s is ID number\n", id)
+        } else {
+            t.Logf("%s is ID number\n", id)
+        }
+    } else {
+        if flag == "0" {
+            t.Logf("%s is not ID number\n", id)
+        } else {
+            t.Errorf("%s is not ID number\n", id)
+        }
+    }
 }

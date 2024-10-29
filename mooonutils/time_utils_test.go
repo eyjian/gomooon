51370b086e6198e725cd672f801cd3d3f89c70a8
jayyi <jayyi@tencent.com>
2024-10-29 19:04:40 +0800

    1、优化mooonutils.NormalizeDateTimeString，以支持将“2024年01月02日12时03分04秒”转为“2024-01-02 12:03:04”
    1、优化mooonutils.NormalizeDateTimeString，以支持将“2024年01月02日12时03分04秒”转为“2024-01-02 12:03:04”

--- mooonutils/time_utils_test.go
+++ mooonutils/time_utils_test.go
@@ -3,49 +3,51 @@
 package mooonutils
 
 import (
-    "testing"
-    "time"
+	"testing"
+	"time"
 )
 
 // go test -v -run="TestGetCurrentTimestamp"
 func TestGetCurrentTimestamp(t *testing.T) {
-    timezone := "Asia/Shanghai"
-    timestamp := GetCurrentTimestamp(timezone)
+	timezone := "Asia/Shanghai"
+	timestamp := GetCurrentTimestamp(timezone)
 
-    if timestamp <= 0 {
-        t.Errorf("GetCurrentTimestamp(%s) = %d; want a positive integer", timezone, timestamp)
-    }
+	if timestamp <= 0 {
+		t.Errorf("GetCurrentTimestamp(%s) = %d; want a positive integer", timezone, timestamp)
+	}
 }
 
 // go test -v -run="TestGetCurrentTimestampString"
 func TestGetCurrentTimestampString(t *testing.T) {
-    timezone := "Asia/Shanghai"
-    timestampStr := GetCurrentTimestampString(timezone)
+	timezone := "Asia/Shanghai"
+	timestampStr := GetCurrentTimestampString(timezone)
 
-    if len(timestampStr) != 10 {
-        t.Errorf("len(GetCurrentTimestampString(%s)) = %d; want 10", timezone, len(timestampStr))
-    }
+	if len(timestampStr) != 10 {
+		t.Errorf("len(GetCurrentTimestampString(%s)) = %d; want 10", timezone, len(timestampStr))
+	}
 }
 
 // go test -v -run="TestNormalizeDateTimeString"
 func TestNormalizeDateTimeString(t *testing.T) {
 	testCases := []struct {
-		dateStr      string
-		expectedStr  string
+		dateStr     string
+		expectedStr string
 	}{
-		{"2024年01月02日", "2024-01-02"},
-		{"2024年01月02日", "2024-01-02"},
-		{"2024年01月02日 12时03分04秒", "2024-01-02 12:03:04"},
-		{"2024年01月02日 12时03分04秒", "2024-01-02 12:03:04"},
-		{"2024年1月2日 12时3分4秒", "2024-1-2 12:3:4"},
+		{"2020-01-02", "2020-01-02 00:00:00"},
+		{"2021年01月02日", "2021-01-02 00:00:00"},
+		{"2022-01-02 12:03:04", "2022-01-02 12:03:04"},
+		{"2023年01月02日 12时03分04秒", "2023-01-02 12:03:04"},
+		{"2024年01月02日12时03分04秒", "2024-01-02 12:03:04"},
+		{"2025/01/02 12:03:04", "2025-01-02 12:03:04"},
+		{"2026/01/02", "2026-01-02 00:00:00"},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.dateStr, func(t *testing.T) {
 			cleanedStr := NormalizeDateTimeString(tc.dateStr, true)
 			if cleanedStr != tc.expectedStr {
-				t.Errorf("CleanDateTimeString(%s) = %s; want %s", tc.dateStr, cleanedStr, tc.expectedStr)
+				t.Errorf("CleanDateTimeString(%s) => %s; want %s", tc.dateStr, cleanedStr, tc.expectedStr)
 			} else {
-				t.Logf("CleanDateTimeString(%s) = %s; want %s", tc.dateStr, cleanedStr, tc.expectedStr)
+				t.Logf("CleanDateTimeString(%s) => %s; want %s", tc.dateStr, cleanedStr, tc.expectedStr)
 			}
 		})
 	}
@@ -53,57 +55,57 @@ func TestNormalizeDateTimeString(t *testing.T) {
 
 // go test -v -run="TestString2Time"
 func TestString2Time(t *testing.T) {
-    testCases := []struct {
-        dateStr      string
-        expectedTime time.Time
-    }{
-        {"2024-01-02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024-1-2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024/01/02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024/1/2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024.01.02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024.1.2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"20240102", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"01-02-24", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"01/02/24", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"02-Jan-2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024-Jan-02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024 Jan 02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"02/Jan/2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"02.Jan.2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024/Jan/02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024.Jan.02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"Sunday, 02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"Sun, 02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"Jan 02, 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"Jan 02 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
-        {"2024 Jan 02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+	testCases := []struct {
+		dateStr      string
+		expectedTime time.Time
+	}{
+		{"2024-01-02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024-1-2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024/01/02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024/1/2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024.01.02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024.1.2", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"20240102", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"01-02-24", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"01/02/24", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"02-Jan-2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024-Jan-02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024 Jan 02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"02/Jan/2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"02.Jan.2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024/Jan/02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024.Jan.02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"Sunday, 02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"Sun, 02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"02 Jan 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"Jan 02, 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"Jan 02 2024", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
+		{"2024 Jan 02", time.Date(2024, 1, 2, 0, 0, 0, 0, time.UTC)},
 		{"2024年02月01日", time.Date(2024, 2, 1, 0, 0, 0, 0, time.UTC)},
-    }
+	}
 
-    for _, tc := range testCases {
-        t.Run(tc.dateStr, func(t *testing.T) {
-            result, err := String2Time(tc.dateStr)
-            if err != nil {
-                t.Errorf("String2Time(%s) returned error: %v", tc.dateStr, err)
-            }
-            if !result.Equal(tc.expectedTime) {
-                t.Errorf("String2Time(%s) = %v; want %v", tc.dateStr, result, tc.expectedTime)
-            } else {
+	for _, tc := range testCases {
+		t.Run(tc.dateStr, func(t *testing.T) {
+			result, err := String2Time(tc.dateStr)
+			if err != nil {
+				t.Errorf("String2Time(%s) returned error: %v", tc.dateStr, err)
+			}
+			if !result.Equal(tc.expectedTime) {
+				t.Errorf("String2Time(%s) = %v; want %v", tc.dateStr, result, tc.expectedTime)
+			} else {
 				t.Logf("%s ok\n", tc.dateStr)
 			}
-        })
-    }
+		})
+	}
 }
 
 // go test -v -run="TestString2Time_InvalidFormat"
 func TestString2Time_InvalidFormat(t *testing.T) {
-    dateStr := "invalid format"
-    _, err := String2Time(dateStr)
+	dateStr := "invalid format"
+	_, err := String2Time(dateStr)
 
-    if err == nil {
-        t.Errorf("String2Time(%s) did not return an error; want an error", dateStr)
-    }
+	if err == nil {
+		t.Errorf("String2Time(%s) did not return an error; want an error", dateStr)
+	}
 }

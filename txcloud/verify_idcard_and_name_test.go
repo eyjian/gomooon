39a1cbc6548f35e1ab1a224eaa118de9e49fead4
jayyi <eyjian@qq.com>
2024-11-16 14:29:02 +0800

    增加身份证和姓名的批量验证函数BatchVerifyIdcardAndName

--- txcloud/verify_idcard_and_name_test.go
+++ txcloud/verify_idcard_and_name_test.go
@@ -7,6 +7,39 @@ import (
 	"testing"
 )
 
+// go test -v -run="TestBatchVerifyIdcardAndName" -args secret_id secret_key
+func TestBatchVerifyIdcardAndName(t *testing.T) {
+	args := os.Args[5:]
+	secretId := args[0]
+	secretKey := args[1]
+
+	data := map[string]*IdcardNameTuple{
+		"123456789012345678": {
+			Idcard: "123456789012345678",
+			Name:   "张三",
+		},
+		"123456789012345679": {
+			Idcard: "123456789012345679",
+			Name:   "李四",
+		},
+		"123456789012345680": {
+			Idcard: "123456789012345680",
+			Name:   "王五",
+		},
+		"123456789012345681": {
+			Idcard: "123456789012345681",
+			Name:   "赵六",
+		},
+	}
+
+	txCloud := NewFace(secretId, secretKey)
+	consistent, inconsistent, fail := txCloud.BatchVerifyIdcardAndName(2, data)
+	t.Logf("consistent:%d, inconsistent:%d, fail:%d\n", consistent, inconsistent, fail)
+	for _, v := range data {
+		t.Logf("%s: %+v\n", v.Idcard, v)
+	}
+}
+
 // go test -v -run="TestVerifyIdcardAndName" -args secret_id secret_key idcard name
 func TestVerifyIdcardAndName(t *testing.T) {
 	args := os.Args[5:]

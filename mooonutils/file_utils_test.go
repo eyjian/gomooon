d863b4a420745175b30363e82ebfc728ea43b894
jayyi <jayyi@tencent.com>
2024-11-28 10:39:35 +0800

    1、增加文件工具函数 mooonutils.DeleteFile 和 mooonutils.CopyFile

--- mooonutils/file_utils_test.go
+++ mooonutils/file_utils_test.go
@@ -17,6 +17,42 @@ func TestMd5File(t *testing.T) {
 	}
 }
 
+// go test -v -run="TestDeleteFile"
+func TestDeleteFile(t *testing.T) {
+	err := DeleteFile("str_utils.copy")
+	if err != nil {
+		t.Errorf("delete %s error: %s\n", "str_utils_copy.go", err.Error())
+	} else {
+		t.Logf("delete %s ok\n", "str_utils_copy.go")
+	}
+
+	err = DeleteFile("str_utils.copy")
+	if err != nil {
+		t.Errorf("delete %s error: %s\n", "str_utils_copy.go", err.Error())
+	} else {
+		t.Logf("delete %s ok\n", "str_utils_copy.go")
+	}
+}
+
+// go test -v -run="TestCopyFile"
+func TestCopyFile(t *testing.T) {
+	srcFile := "str_utils.go"
+	dstFile := "str_utils.copy"
+	err := CopyFile(srcFile, dstFile, true)
+	if err != nil {
+		t.Errorf("copy %s to %s error: %s\n", srcFile, dstFile, err.Error())
+	} else {
+		t.Logf("copy %s to %s ok\n", srcFile, dstFile)
+	}
+
+	err = CopyFile(srcFile, dstFile, false)
+	if err != nil {
+		t.Errorf("copy %s to %s error: %s\n", srcFile, dstFile, err.Error())
+	} else {
+		t.Logf("copy %s to %s ok\n", srcFile, dstFile)
+	}
+}
+
 // go test -v -run="TestUnzip"
 func TestUnzip(t *testing.T) {
 	destDir, err := os.Getwd()

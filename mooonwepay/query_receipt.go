bd265671d4fb5cf7524173491eb1f8ae56e6eb9d
jayyi <jayyi@tencent.com>
2025-03-27 19:49:45 +0800

    新增微信支付的申请电子回单和查询电子回单接口实现
    官方文档：https://pay.weixin.qq.com/doc/v3/merchant/4012716452

---
+++ mooonwepay/query_receipt.go
@@ -0,0 +1,64 @@
+// Package mooonwepay
+// Wrote by yijian on 2025/03/27
+package mooonwepay
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/wechatpay-apiv3/wechatpay-go/core"
+)
+
+// QueryReceiptRequest 查询电子回单请求结构体
+type QueryReceiptRequest struct {
+	ctx         context.Context
+	OutBillNo   string `json:"out_bill_no"`   // 商户单号
+	WepayBillNo string `json:"wepay_bill_no"` // 微信单号
+}
+
+// QueryReceiptResponse 查询电子回单响应结构体
+type QueryReceiptResponse struct {
+	State       string `json:"state"`
+	CreateTime  string `json:"create_time"`
+	UpdateTime  string `json:"update_time"`
+	HashType    string `json:"hash_type"`
+	HashValue   string `json:"hash_value"`
+	DownloadUrl string `json:"download_url"`
+}
+
+// QueryReceipt 查询电子回单
+func QueryReceipt(client *core.Client, req *QueryReceiptRequest) (*QueryReceiptResponse, error) {
+	// 定义请求 URL
+	var url string
+	if req.OutBillNo != "" {
+		url = fmt.Sprintf("https://api.mch.weixin.qq.com/v3/fund-app/mch-transfer/elecsign/out-bill-no/%s", req.OutBillNo)
+	} else if req.WepayBillNo != "" {
+		url = fmt.Sprintf("https://api.mch.weixin.qq.com/v3/fund-app/mch-transfer/elecsign/transfer-bill-no/%s", req.WepayBillNo)
+	} else {
+		return nil, fmt.Errorf("QueryReceipt with invalid request: out_bill_no and wepay_bill_no are both empty")
+	}
+
+	// 发送 GET 请求
+	apiResult, err := client.Get(req.ctx, url)
+	if err != nil {
+		return nil, fmt.Errorf("QueryReceipt failed to apply electronic receipt: %w", err)
+	}
+
+	// 读取响应体内容
+	defer apiResult.Response.Body.Close()
+	respBody, err := io.ReadAll(apiResult.Response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("QueryReceipt failed to read response body: %w", err)
+	}
+
+	// 解析响应
+	var resp QueryReceiptResponse
+	if err := json.Unmarshal(respBody, &resp); err != nil {
+		return nil, fmt.Errorf("QueryReceipt failed to unmarshal response: %w", err)
+	}
+
+	// 返回响应
+	return &resp, nil
+}
---
+++ mooonwepay/query_receipt_test.go
@@ -0,0 +1,56 @@
+// Package mooonwepay
+// Wrote by yijian on 2025/03/27
+package mooonwepay
+
+import (
+	"context"
+	"github.com/wechatpay-apiv3/wechatpay-go/core"
+	"github.com/wechatpay-apiv3/wechatpay-go/core/option"
+	"github.com/wechatpay-apiv3/wechatpay-go/utils"
+	"os"
+	"testing"
+)
+
+// go test -v -run="TestQueryReceipt" -args mchid serial_no mch_api_v3_key private_key wepay_bill_no
+func TestQueryReceipt(t *testing.T) {
+	args := os.Args[5:]
+	if len(args) != 5 {
+		t.Fatal("args num error")
+	}
+	mchID := args[0]
+	mchCertificateSerialNumber := args[1]
+	mchAPIv3Key := args[2]
+	privateKey := args[3]
+	wepayBillNo := args[4]
+
+	// 使用 utils 提供的函数从本地文件中加载商户私钥，商户私钥会用来生成请求的签名
+	mchPrivateKey, err := utils.LoadPrivateKeyWithPath(privateKey)
+	if err != nil {
+		t.Errorf("Load merchant private key error: %+v", err)
+		return
+	}
+
+	ctx := context.Background()
+	// 使用商户私钥等初始化 client，并使它具有自动定时获取微信支付平台证书的能力
+	opts := []core.ClientOption{
+		option.WithWechatPayAutoAuthCipher(mchID, mchCertificateSerialNumber, mchPrivateKey, mchAPIv3Key),
+	}
+	client, err := core.NewClient(ctx, opts...)
+	if err != nil {
+		t.Errorf("new wechat pay client err:%s", err)
+		return
+	}
+
+	// 调用查询电子回单的函数
+	resp, err := QueryReceipt(client, &QueryReceiptRequest{
+		ctx:         context.Background(),
+		OutBillNo:   "",
+		WepayBillNo: wepayBillNo,
+	})
+	if err != nil {
+		t.Errorf("查询电子回单时出错: %+v", err)
+		return
+	}
+
+	t.Logf("查询电子回单成功: %+v", resp)
+}

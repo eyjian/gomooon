32ae83710a67a2478877a990d5ab623d383bf9f8
岚天逸剑 <eyjian@qq.com>
2025-06-14 12:25:01 +0800

    优化目录工具函数的实现

--- mooonutils/dir_utils_test.go
+++ mooonutils/dir_utils_test.go
@@ -4,21 +4,50 @@ package mooonutils
 
 import "testing"
 
-// go test -v -run="TestDirExists"
-func TestDirExists(t *testing.T) {
-	dir := "./tmp"
-	exists, err := DirExists(dir)
-	if err != nil {
-		t.Log(err)
-	} else {
-		t.Errorf("dir exists: %v", exists)
-	}
-
-	dir = "."
-	exists, err = DirExists(dir)
+// go test -v -run="TestPathExists"
+func TestPathExists(t *testing.T) {
+	// 不存在的
+	dir := "acd"
+	exists, isDir, err := PathExists(dir)
 	if err != nil {
 		t.Error(err)
-		return
+	} else {
+		if !exists {
+			t.Log("not exists")
+		} else if isDir {
+			t.Log("dir exists")
+		} else {
+			t.Log("file exists")
+		}
 	}
-	t.Logf("dir `%s` exists: %v", dir, exists)
+
+	// 存在的目录
+	dir = "."
+	exists, isDir, err = PathExists(dir)
+	if err != nil {
+		t.Error(err)
+	} else {
+		if !exists {
+			t.Log("not exists")
+		} else if isDir {
+			t.Log("dir exists")
+		} else {
+			t.Log("file exists")
+		}
+	}
+
+	// 存在的文件
+	dir = "dir_utils.go"
+	exists, isDir, err = PathExists(dir)
+	if err != nil {
+		t.Error(err)
+	} else {
+		if !exists {
+			t.Log("not exists")
+		} else if isDir {
+			t.Log("dir exists")
+		} else {
+			t.Log("file exists")
+		}
+	}
 }
